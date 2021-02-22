e580960610d382090d6badadffa66796febfa4d3
Massimiliano Scarpetta <massy1997.lavoro@gmail.com>
2021-02-22 23:14:52 +0100

    Creato scena principale e Script di benvenuto

---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+		int max = 1000;
+		int min = 0;
+
+		Debug.Log("Benvenuto Viaggiatore! Io sono il mago dei numeri...");
+		Debug.Log("Ora avvicinati a me e guardami dritto negli occhi mentre pensi ad un numero");
+		Debug.Log("Ricorda però che i miei poteri sono limitati e non puoi dunque pensare ad un numero maggiore di " + max + "...");
+		Debug.Log("o ad uno minore di " + min + "... Iniziamo!!!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
