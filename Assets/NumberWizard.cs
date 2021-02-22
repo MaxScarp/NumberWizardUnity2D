51ea41e135404a3602a8be94e8c110ec0ea8f3fd
Massimiliano Scarpetta <massy1997.lavoro@gmail.com>
2021-02-22 23:47:59 +0100

    Aggiunti metodi che rilevano se il pulsante Up,Down,Enter vengono premuti su tastiera.

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -14,10 +14,24 @@ public class NumberWizard : MonoBehaviour {
 		Debug.Log("Ora avvicinati a me e guardami dritto negli occhi mentre pensi ad un numero");
 		Debug.Log("Ricorda però che i miei poteri sono limitati e non puoi dunque pensare ad un numero maggiore di " + max + "...");
 		Debug.Log("o ad uno minore di " + min + "... Iniziamo!!!");
+		Debug.Log("Dimmi se il numero che hai pensato è minore o maggiore di 500");
+		Debug.Log("Freccia su = Maggiore; Freccia giù = Minore; Enter = Corretto");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+		if (Input.GetKeyDown(KeyCode.UpArrow))
+		{
+			Debug.Log("Up Arrow key was pressed.");
+		}
+		if(Input.GetKeyDown(KeyCode.DownArrow))
+        {
+			Debug.Log("Down Arrow key was pressed.");
+		}
+		if(Input.GetKeyDown(KeyCode.Return))
+        {
+			Debug.Log("Enter key was pressed.");
+		}
 	}
 }
